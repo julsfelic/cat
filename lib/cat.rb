a223e8efb077edfdce0d2315dd45b4538702a3d7
Julian Feliciano <julsfelic@gmail.com>
2016-01-02 11:52:02 -0700

    Finish cat and fix typos in tests

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,2 +1,26 @@
-cat = Cat.new(ARGV)
-cat.print_contents
+class Cat
+  attr_reader :args
+
+  def initialize(args)
+    @args = args
+  end
+
+  def format_content(filename)
+    File.read(filename)
+  end
+
+  def format_files
+    args.map do |arg|
+      format_content(arg)
+    end.join
+  end
+
+  def print_contents
+    print format_files
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  cat = Cat.new(ARGV)
+  cat.print_contents
+end
--- test/cat_test.rb
+++ test/cat_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
+require 'cat'
 
 class CatAcceptanceTest < Minitest::Test
   def test_prints_out_the_contents_of_a_single_file
     printed = `ruby lib/cat.rb boring_work_stuff.md`
     expected = "My file extension is \"md\"\n" +
                "that means \"markdown\" to nearly everybody.\n" +
-               "But that's my trick to diverty inquiry...\n" +
-               "for me, \"md\" implies \"my diary\""
+               "But that's my trick to divert inquiry...\n" +
+               "for me, \"md\" implies \"my diary\"\n"
 
     assert_equal expected, printed
   end
@@ -15,23 +16,27 @@ class CatAcceptanceTest < Minitest::Test
     printed = `ruby lib/cat.rb boring_work_stuff.md lib/myfile.txt`
     expected = "My file extension is \"md\"\n" +
                "that means \"markdown\" to nearly everybody.\n" +
-               "But that's my trick to diverty inquiry...\n" +
+               "But that's my trick to divert inquiry...\n" +
                "for me, \"md\" implies \"my diary\"\n" +
                "i am quite sure\n" +
                "that within lib\n" +
                "resides myfile,\n" +
-               "patiently immobile"
+               "patiently immobile\n"
 
     assert_equal expected, printed
   end
 end
 
 class CatTest < Minitest::Test
-  def test_receives_one_file_and_formats_output
-    skip
+  def test_formats_one_file
+    cat = Cat.new(['boring_work_stuff.md'])
+
+    assert_equal 4, cat.format_content('boring_work_stuff.md').lines.count
   end
 
   def test_receives_two_files_and_formats_output
-    skip
+    cat = Cat.new(['boring_work_stuff.md', 'lib/myfile.txt'])
+
+    assert_equal 8, cat.format_files.lines.count
   end
 end
