331f0ec4372900f3bf5faf1667c5386e8946a64b
Julian Feliciano <julsfelic@gmail.com>
2016-01-02 10:54:23 -0700

    Add acceptance tests for cat

--- test/cat_test.rb
+++ test/cat_test.rb
@@ -1,1 +1,27 @@
 require 'test_helper'
+
+class CatAcceptanceTest < Minitest::Test
+  def test_prints_out_the_contents_of_a_single_file
+    printed = `ruby lib/cat.rb boring_work_stuff.md`
+    expected = "My file extension is \"md\"\n" +
+               "that means \"markdown\" to nearly everybody.\n" +
+               "But that's my trick to diverty inquiry...\n" +
+               "for me, \"md\" implies \"my diary\""
+
+    assert_equal expected, printed
+  end
+
+  def test_prints_out_the_contents_of_two_files
+    printed = `ruby lib/cat.rb boring_work_stuff.md lib/myfile.txt`
+    expected = "My file extension is \"md\"\n" +
+               "that means \"markdown\" to nearly everybody.\n" +
+               "But that's my trick to diverty inquiry...\n" +
+               "for me, \"md\" implies \"my diary\"\n" +
+               "i am quite sure\n" +
+               "that within lib\n" +
+               "resides myfile,\n" +
+               "patiently immobile"
+
+    assert_equal expected, printed
+  end
+end
